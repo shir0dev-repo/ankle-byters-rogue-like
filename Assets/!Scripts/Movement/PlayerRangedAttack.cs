c636ea22e3f05c76d63db7ee6b22329c8a7701a3
Dot1298 <148091241+Dot1298@users.noreply.github.com>
2024-01-18 16:26:43 -0500

    RangedAttacks using controller

--- Assets/!Scripts/Movement/PlayerRangedAttack.cs
+++ Assets/!Scripts/Movement/PlayerRangedAttack.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
+[RequireComponent(typeof(PlayerInputHandler))]
 public class PlayerRangedAttack : MonoBehaviour
 {
     [SerializeField] GameObject _projectilePrefab;
+    [SerializeField] float fireCooldown = 0.5f; // Adjust the cooldown time as needed
 
+    private float nextFireTime;
+    private const string _RANGEDATTACKS_ACTION_NAME = "RangedAttack";
+    private InputAction _rangedAttack;
+    private PlayerInputHandler _playerInputHandler;
 
-    private void Update()
+    private void Awake()
     {
-        if (Input.GetMouseButtonDown(1))
+        _playerInputHandler = GetComponent<PlayerInputHandler>();
+        _rangedAttack = _playerInputHandler.PlayerActions.FindAction(_RANGEDATTACKS_ACTION_NAME);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rangedAttack.ReadValue<float>() > 0.5f && Time.time >= nextFireTime)
         {
             Instantiate(_projectilePrefab, transform.position, transform.rotation);
+            nextFireTime = Time.time + fireCooldown;
         }
     }
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(1))
+    //    {
+    //        Instantiate(_projectilePrefab, transform.position, transform.rotation);
+    //    }
+    //}
 }
