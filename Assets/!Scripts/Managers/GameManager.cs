4a2fb9fdfd17445af3bebeb9320db279dd36306a
James Mitchell <v010000010101001101010011@gmail.com>
2024-01-21 21:06:44 -0500

    added singleton abstract class
    player is no longer present in scene, and spawns in on gamemanager.Awake()

--- Assets/!Scripts/Managers/GameManager.cs
+++ Assets/!Scripts/Managers/GameManager.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private GameObject _playerPrefab;
     private Transform _playerTransform;
 
     public Vector3 PlayerPosition => _playerTransform.position;
 
-    private void Awake()
+    protected override void Awake()
     {
         _playerTransform = Instantiate(_playerPrefab, Vector3.zero, Quaternion.identity).transform;
     }
---
+++ Assets/!Scripts/Utils/Singleton.cs
@@ -0,0 +1,47 @@
+// Taken from Tarodev's youtube video about Unity Architecture: https://www.youtube.com/watch?v=tE1qH8OxO2Y.
+
+using UnityEngine;
+
+/// <summary>
+/// Create static instance of T, overwriting any previous instance.
+/// Does not destroy T.
+/// </summary>
+public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+    protected virtual void Awake() => Instance = this as T;
+
+    protected virtual void OnApplicationQuit()
+    {
+        Instance = null;
+        Destroy(gameObject);
+    }
+}
+
+/// <summary>
+/// Creates a singleton instance of T.
+/// Destroys self if T instance is not null.
+/// </summary>
+public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        if (Instance == null) Destroy(gameObject);
+        base.Awake();
+    }
+}
+
+/// <summary>
+/// Creates a singleton instance of T.
+/// Destroys self if T instance is not null.
+/// Persists upon loading a new scene.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+}
