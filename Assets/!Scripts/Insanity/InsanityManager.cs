3870d6a60cedd792aa85a67f80a7d480f277e642
James Mitchell <v010000010101001101010011@gmail.com>
2024-01-25 12:49:17 -0500

    added timetick system and fixed singleton bug

--- Assets/!Scripts/Insanity/InsanityManager.cs
+++ Assets/!Scripts/Insanity/InsanityManager.cs
@@ -9,7 +9,28 @@ public class InsanityManager : Singleton<InsanityManager>
     [SerializeField] private int _currentInsanity = 0;
     [SerializeField] private int _maxInsanity = 100;
 
+    [SerializeField] private int _insanityIncrement;
+    [SerializeField] private bool _incrementOverTime = false;
+
     public Action<int> OnInsanityChanged;
+
+    private void OnEnable()
+    {
+        TimeTickSystem.OnTick_5 += AddInsanityRecursive;
+    }
+
+    private void OnDisable()
+    {
+        TimeTickSystem.OnTick_5 -= AddInsanityRecursive;
+    }
+
+    private void AddInsanityRecursive(object sender, TimeTickSystem.OnTickChangedArgs e)
+    {
+        if (_incrementOverTime == false) return;
+
+        Debug.Log($"Increasing sanity at tick {e.CurrentTick} by {_insanityIncrement}.");
+        AddInsanity(_insanityIncrement);
+    }
 
     public void AddInsanity(int insanityAmount)
     {
