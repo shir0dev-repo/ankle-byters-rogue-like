a4d4f9f4d60473645b3af9239b0808906c9527af
James Mitchell <v010000010101001101010011@gmail.com>
2024-01-28 12:36:39 -0500

    added camera manager and adjusted door

--- Assets/!Scripts/Door/Door.cs
+++ Assets/!Scripts/Door/Door.cs
@@ -3,53 +3,23 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject door;
-    public Camera mainCamera;
+    //instead of storing the player, use a layermask for it
+    [SerializeField] private LayerMask _playerLayermask;
 
-    public Vector3 playerPosition;
-    public Vector3 doorLocation;
-    public Vector3 cameraLocation;
-    void Start()
+    public Vector3 GetAdjacentRoom()
     {
-        if (player != null && door != null)
-        {
-            doorLocation = door.transform.position;
-        }
-        else
-        {
-            Debug.LogError("Player or door not assigned to the Door script! Assign both in the Unity Editor.");
-        }
+        Vector3 cameraPos = Camera.main.transform.position;
+        cameraPos.z = 0;
+        
+        Vector3 directionFromCamera = (transform.position - cameraPos).normalized;
+
+        return Vector3.Scale((Vector3)CameraManager.ScreenExtents, directionFromCamera) * 2f;
     }
 
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(mainCamera != null)
-        {
-            cameraLocation = mainCamera.transform.position;
-        }
-        if (player != null)
-        {
-            playerPosition = player.transform.position;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            Debug.Log("A New Room!");
-
-            if (cameraLocation.x < doorLocation.x)
-            {
-                Debug.Log("Welcome to Room 2");
-                Camera.main.transform.position = new Vector3(13.4f, 0f, -1f);
-            }
-            else
-            {
-                Debug.Log("Welcome to Room 1");
-                Camera.main.transform.position = new Vector3(0f, 0f, -1f);
-            }
-        }
+        if (!_playerLayermask.IsLayer(collision.gameObject.layer)) return;
+        
+        GameManager.Instance.CameraManager.EnterRoom(this);
     }
 }
