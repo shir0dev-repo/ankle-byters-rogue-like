69937770c189f7db8ce67d616eec899079fbec93
Dot1298 <148091241+Dot1298@users.noreply.github.com>
2024-02-05 14:07:01 -0500

    Enemies are good, but win image is dumb

--- Assets/!Scripts/Enemy/EnemyBasic/BasicEnemy.cs
+++ Assets/!Scripts/Enemy/EnemyBasic/BasicEnemy.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicEnemy : MonoBehaviour
 {
     [SerializeField] float _speed;
+    [SerializeField] float _seekRange = 4f;
 
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, GameManager.Instance.PlayerManager.GetPlayerPosition(), _speed * Time.deltaTime);
+        Vector3 playerPosition = GameManager.Instance.PlayerManager.GetPlayerPosition();
+        float distanceToPlayer = Vector3.Distance(transform.position, playerPosition);
+
+        if (distanceToPlayer <= _seekRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerPosition, _speed * Time.deltaTime);
+        }
     }
 }
---
+++ Assets/!Scripts/Managers/EnemySpawner.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject bossPrefab;
+    [SerializeField] GameObject winImage;
+    
+    bool winImageSpawned = false;
+
+    float minX = -26f;
+    float maxX = -9f;
+    float minY = 0f;
+    float maxY = 5f;
+
+    GameObject bossInstance;
+    Health bossHealth;
+
+    private void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+            Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+        }
+        bossInstance = Instantiate(bossPrefab, new Vector3(-36f, 0f, 0f), Quaternion.identity);
+
+        bossHealth = bossInstance.GetComponent<Health>();
+    }
+
+    private void Update()
+    {
+        if (bossHealth.CurrentHealth <= 0 && !winImageSpawned)
+        {
+            Debug.Log("Boss defeated!");
+            if (winImage != null)
+            {
+                winImage.SetActive(true);
+                Instantiate(winImage, new Vector3(-1518f, -4f, 0f), Quaternion.identity);
+                Debug.Log("You win!");
+                winImageSpawned = true;
+            }
+        }
+    }
+}
