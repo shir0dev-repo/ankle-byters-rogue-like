4c72f8773703cfdb88dc287d14d231e6009456f4
Lizoid66 <mdingelstadmoore@gmail.com>
2024-02-27 16:19:31 -0500

    SpawnShadow
    ShadowManager now spawns 10 instances of TestShadow for testing

--- Assets/!Scripts/Enemy/InsanityEffects/Shadows/ShadowManager.cs
+++ Assets/!Scripts/Enemy/InsanityEffects/Shadows/ShadowManager.cs
@@ -8,9 +8,12 @@ public class ShadowManager : MonoBehaviour
     List<GameObject> shadowPassive = new List<GameObject>();
     //Shadows looking at the player
     List<GameObject> shadowLooking = new List<GameObject>();
+
+    [SerializeField] GameObject shadowPrefab;
     void Start()
     {
         InsanityManager.OnInsanityChanged += ShadowBehaviour;
+        SpawnShadows();
     }
 
     // Update is called once per frame
@@ -22,5 +25,15 @@ public class ShadowManager : MonoBehaviour
     void ShadowBehaviour(int insanity)
     {
         
+    }
+
+    void SpawnShadows()
+    {
+        Vector3 testSpawn = new Vector3(-8f, -3f);
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate<GameObject>(shadowPrefab, testSpawn, Quaternion.identity, transform);
+            testSpawn = new Vector3(testSpawn.x + 1.5f, -3f);
+        }
     }
 }
