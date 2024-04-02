da7bc233df10b1ec90ffd98a17b426cdd6bf13f5
Lizoid66 <mdingelstadmoore@gmail.com>
2024-04-02 00:55:52 -0400

    Update ShadowScript.cs

--- Assets/!Scripts/Enemy/Shadows/ShadowScript.cs
+++ Assets/!Scripts/Enemy/Shadows/ShadowScript.cs
@@ -38,9 +38,9 @@ public class ShadowScript : MonoBehaviour
 
         transform.Rotate(Vector3.forward, rotationAmount);
         if (isDrifting && ((player.transform.position - transform.position).magnitude < 2 && (player.transform.position - transform.position).magnitude > 1.05))
-        {
-            transform.parent.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.2f * Time.deltaTime);
-        }
+           transform.parent.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.2f * Time.deltaTime);
+        
+        
     }
 
     public void MakeLooker()
